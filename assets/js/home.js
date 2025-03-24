2b7a17a0c22ceea879895c561c6630c6e78c4d6c
abinash <abinash@proflujo.com>
2025-03-24 10:01:53 +0530

    update a personal site

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,24 +1,61 @@
+// $(document).ready(function() {
+//     function printTextOneByOne(selector, text, delay, callback) {
+//         let i = 0;
+//         function typeChar() {
+//             if (i < text.length) {
+//                 $(selector).append(text[i]);
+//                 i++;
+//                 setTimeout(typeChar, delay);
+//             } else if (callback) {
+//                 callback(); // Call the next action when the text is done printing
+//             }
+//         }
+//         typeChar();
+//     }
+
+//     const textToPrint = "Abinash M";
+//     const childText = "I'm a full stack developer.";
+//     const delay = 200; // Delay in milliseconds
+
+//     // First print the main site name, then print the child site name
+//     printTextOneByOne(".main_sitename", textToPrint, delay, function() {
+//         printTextOneByOne(".child_sitename", childText, delay);
+//     });
+// });
+
 $(document).ready(function() {
     function printTextOneByOne(selector, text, delay, callback) {
         let i = 0;
+        $(selector).html("");
         function typeChar() {
             if (i < text.length) {
                 $(selector).append(text[i]);
                 i++;
                 setTimeout(typeChar, delay);
             } else if (callback) {
-                callback(); // Call the next action when the text is done printing
+                setTimeout(callback, 1000);
             }
         }
         typeChar();
     }
 
-    const textToPrint = "Abinash M";
-    const childText = "I'm a full stack developer.";
-    const delay = 200; // Delay in milliseconds
+    const texts = [
+        "I'm a Full-Stack Developer.",
+        "I'm a Senior Software Engineer.",
+        "Web & Mobile Application Developer.",
+        "Freelance web developer available for Web Application, Deployment & Maintenance."
+    ];
 
-    // First print the main site name, then print the child site name
-    printTextOneByOne(".main_sitename", textToPrint, delay, function() {
-        printTextOneByOne(".child_sitename", childText, delay);
-    });
+    let currentIndex = 0;
+    const delay = 100;
+
+    function cycleTexts() {
+        printTextOneByOne(".child_sitename", texts[currentIndex], delay, function() {
+            currentIndex = (currentIndex + 1) % texts.length;
+            setTimeout(cycleTexts, 200);
+        });
+    }
+
+    $(".main_sitename").text("Abinash M");
+    cycleTexts();
 });
